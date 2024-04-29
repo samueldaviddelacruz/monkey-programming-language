ca1bb2d8f92bc5187eca0f489a9369c8883048e8
Samuel De La Cruz <samuel@codetiger.co>
2024-04-29 08:00:34 -0400

    Add CallExpression parsing

--- parser/parser.go
+++ parser/parser.go
@@ -28,6 +28,7 @@ var precedences = map[token.TokenType]int{
 	token.MINUS:    SUM,
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
+	token.LPAREN:   CALL,
 }
 
 type (
@@ -73,6 +74,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 
 	return p
 }
@@ -156,42 +158,6 @@ func (p *Parser) parseIfExpression() ast.Expression {
 	return expression
 }
 
-/*
-	“func (p *Parser) parseFunctionParameters() []*ast.Identifier {
-	    identifiers := []*ast.Identifier{}
-
-	    if p.peekTokenIs(token.RPAREN) {
-	        p.nextToken()
-	        return identifiers
-	    }
-
-	    p.nextToken()
-
-	    ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
-	    identifiers = append(identifiers, ident)
-
-	    for p.peekTokenIs(token.COMMA) {
-	        p.nextToken()
-	        p.nextToken()
-	        ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
-	        identifiers = append(identifiers, ident)
-	    }
-
-	    if !p.expectPeek(token.RPAREN) {
-	        return nil
-	    }
-
-	    return identifiers
-	}
-
-”
-
-Excerpt From
-Writing An Interpreter In Go
-Thorsten Ball
-https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=0
-This material may be protected by copyright.
-*/
 func (p *Parser) parseFunctionLiteral() ast.Expression {
 	lit := &ast.FunctionLiteral{Token: p.curToken}
 
@@ -208,6 +174,31 @@ func (p *Parser) parseFunctionLiteral() ast.Expression {
 	lit.Body = p.parseBlockStatement()
 
 	return lit
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.curToken, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+	return args
 }
 
 func (p *Parser) parseFunctionParameters() []*ast.Identifier {
