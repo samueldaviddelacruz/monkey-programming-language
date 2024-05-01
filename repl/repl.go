7872c6b524d8b589db8314c16b5bf73c1743e89b
Samuel De La Cruz <samuel@codetiger.co>
2024-05-01 08:00:45 -0400

    add print of program to repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -21,8 +21,19 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+		fmt.Fprintln(out, program.String())
+	}
+
+}
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		fmt.Fprintln(out, "parser errors: ")
+		fmt.Fprintf(out, "\t %s \n", msg)
 	}
 }
