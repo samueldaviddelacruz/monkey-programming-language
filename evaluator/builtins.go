f26a82b3602173415980c2034846bdd08d4e37d1
Samuel De La Cruz <samuel@codetiger.co>
2024-05-11 21:42:34 -0400

    .

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -72,7 +72,7 @@ var builtins = map[string]*object.Builtin{
 			if len(args) != 2 {
 				return newError("wrong number of arguments. got=%d, want=2", len(args))
 			}
-			if args[0].Type() != object.ERROR_OBJ {
+			if args[0].Type() != object.ARRAY_OBJ {
 				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
 			}
 			arr := args[0].(*object.Array)
